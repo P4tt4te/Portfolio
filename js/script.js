314b09bf924913a7ae33bbaafa50671be5929b2b
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-04-29 16:37:34 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// mise en place du curseur
+
+var hoverables = document.querySelectorAll('.hoverable');
+
+document.addEventListener('DOMContentLoaded', souris);
+
+function souris( evt ) {
+  const grandCercle = document.querySelector('.cursor2');
+  const petitCercle = document.querySelector('.cursor');
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    let mousePosX = 0,
+        mousePosY = 0;
+
+    document.addEventListener('mousemove',coor);
+    function coor( e ) {
+      mousePosX = e.pageX;
+      mousePosY = e.pageY;
+      petitCercle.style.top = mousePosY + "px";
+      petitCercle.style.left = mousePosX + "px";
+    }
+
+    let delay = 6,
+        calculMousePosX = 0,
+        calculMousePosY = 0;
+
+    function delayMouseFollow() {
+        requestAnimationFrame(delayMouseFollow);
+
+        calculMousePosX += (mousePosX - calculMousePosX) / delay;
+        calculMousePosY += (mousePosY - calculMousePosY) / delay;
+
+        grandCercle.style.top = calculMousePosY + "px";
+        grandCercle.style.left = calculMousePosX + "px";
+    }
+    delayMouseFollow();
+  }
+
+}
