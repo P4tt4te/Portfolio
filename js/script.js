915f8b45590c034bc053c566df48eb361a78c607
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-05-01 00:18:03 +0200

    Béta 30/05 fin
    -ajout du desciptif de l'image

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@
 var hoverables = document.querySelectorAll('.hoverable');
 
 document.addEventListener('DOMContentLoaded', debut);
-
+//fonction general//
 function debut(evt) {
   souris();
   carte();
   carteanim();
-}
 
+}
+// gere l'affichage du pointeur sur pc//
 function souris() {
   const grandCercle = document.querySelector('.cursor2');
   const petitCercle = document.querySelector('.cursor');
@@ -49,7 +50,7 @@ function souris() {
   }
 }
 
-
+// gere le retournement de la carte//
 function carte() {
   var carte = document.querySelector('.ensemble-carte');
 
@@ -60,25 +61,27 @@ function carte() {
   }
 
 }
-
+//détermine si l'utilisateur est sur pc ou smartphone//
 function carteanim() {
   console.log('carteanim');
   if (window.matchMedia("(min-width: 768px)").matches) {
     coordsouris();
+    descimage(1);
   } else {
     coordtel();
+    descimage(0);
   }
 
 
 }
-
+//gere les coordonnées sur pc (en fonction de la postion de la souris)//
 function coordsouris() {
   console.log('souris');
   var flip = document.querySelector('.carte-flip');
   var carte = document.querySelector('.ensemble-carte');
   let mousePosX = 0,
-      largeur,
-      longeur,
+    largeur,
+    longeur,
     mousePosY = 0;
   document.addEventListener('mousemove', coord);
 
@@ -89,8 +92,8 @@ function coordsouris() {
     largeur = window.innerWidth;
     longeur = window.innerHeight;
 
-    mousePosX = ((mousePosX * 2) - largeur)/40;
-    mousePosY = ((mousePosY * 2) - longeur)/40;
+    mousePosX = ((mousePosX * 2) - largeur) / 40;
+    mousePosY = ((mousePosY * 2) - longeur) / 40;
 
     if (carte.classList.contains('retourne') == true) {
       mousePosX = mousePosX + 180;
@@ -108,7 +111,7 @@ function coordsouris() {
 
 
 }
-
+//gere les coordonnées sur téléphone (avec le gyroscope)//
 function coordtel() {
   console.log('tel');
   var flip = document.querySelector('.carte-flip');
@@ -127,6 +130,17 @@ function coordtel() {
     }
     flip.style.transform = 'rotateX(' + mousePosY + "deg)" + ' rotateY(' + mousePosX + 'deg)';
   }
+}
 
+function descimage(type) {
+  var zone = document.querySelector('.desc-carte-txt');
+  let texte = document.createElement('p');
 
+  if (type > 0) {
+    texte.textContent = "Clique sur l'image ;";
+  } else {
+    texte.textContent = "Tappote l'image ;";
+  }
+  texte.classList.add('texte-clique');
+  zone.prepend(texte);
 }
